ae04a3677519ff3c29e0da7c5486ae91d346389f
Jinguo Liu <cacate0129@gmail.com>
2024-10-04 23:30:50 +0800

    new, set enumeration (#7)

--- src/algorithms/mis1.jl
+++ src/algorithms/mis1.jl
@@ -1,4 +1,4 @@
-export mis1
+export mis1, mis1e
 
 """
 Solving MIS problem with simple branching algorithm.
@@ -10,5 +10,36 @@ function mis1(eg::EliminateGraph)
     else
         vmin, dmin = mindegree_vertex(eg)
         return 1 + neighborcover_mapreduce(y->eliminate(mis1, eg, NeighborCover(y)), max, eg, vmin)
+    end
+end
+
+"""
+    mis1e(eg::EliminateGraph)
+
+Enumerate all MISs of a graph. Returns the size of the MIS and the MISs.
+"""
+function mis1e(eg::EliminateGraph)
+    N = nv(eg)
+    if N == 0
+        return 0, push!(Set{Set{Int}}(), Set{Int}())
+    else
+        vmin, dmin = mindegree_vertex(eg)
+        alpha =  typemin(Int)
+        sets = Set{Set{Int}}()
+        for v in neighborcover(eg, vmin)
+            subgraph = eliminate(eg, NeighborCover(v))
+            alphav, setsv = mis1e(subgraph)
+            if alphav > alpha
+                alpha = alphav
+                empty!(sets)
+            end
+            if alphav == alpha
+                for s in setsv
+                    push!(s, v)
+                    push!(sets, s)
+                end
+            end
+        end
+        return 1 + alpha, sets
     end
 end
--- test/algorithms/mis1.jl
+++ test/algorithms/mis1.jl
@@ -1,4 +1,4 @@
-using EliminateGraphs
+using EliminateGraphs, EliminateGraphs.Graphs
 using Test
 
 @testset "mis1" begin
@@ -11,4 +11,24 @@ using Test
                             0 0 0 1 0])
 
     @test mis1(graph) == 3
+end
+
+@testset "mis1e" begin
+    graph = EliminateGraph([0 1 0 0 0;
+                            1 0 1 1 0;
+                            0 1 0 1 0;
+                            0 1 1 0 1;
+                            0 0 0 1 0])
+
+    alpha, sets = mis1e(graph)
+    @test alpha == 3
+    sets = sort(sort.(collect.(sets)))
+    @test sets == [[1, 3, 5]]
+
+    graph = EliminateGraph(smallgraph(:petersen))
+    alpha, sets = mis1e(graph)
+    @test alpha == 4
+    @test length(sets) == 5
+    sets = sort(sort.(collect.(sets)))
+    @test sets == [[1, 3, 9, 10], [1, 4, 7, 8], [2, 4, 6, 10], [2, 5, 8, 9], [3, 5, 6, 7]]
 end
